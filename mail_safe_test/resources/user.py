58bf3705bf0df79d845be03b2f04f451c275593c
Gary Belvin <gdbelvin@gmail.com>
2014-07-03 16:41:01 -0700

    Added admin controls

--- mail_safe_test/resources/user.py
+++ mail_safe_test/resources/user.py
@@ -24,8 +24,6 @@ class NDBUrl(fields.Url):
         except TypeError as te:
             raise MarshallingException(te)
 
-
-
 # Public exports
 user_fields = {
     'first_name': fields.String,
@@ -45,27 +43,37 @@ parser.add_argument('Authorization', type=str, required=True, location='headers'
                             dest='oauth')
 
 class UserModel(ndb.Model):
-    # id - in this key
     first_name = ndb.StringProperty()
     last_name = ndb.StringProperty()
     email = ndb.StringProperty()
     oauth = ndb.StringProperty()
+    admin = ndb.BooleanProperty(default=False)
     created = ndb.DateTimeProperty(auto_now_add=True)
     last_active = ndb.DateTimeProperty(auto_now_add=True)
 
 def get_user(request):
     # TODO(gdb): Verify authorization header loop up user.
     user_id = request.headers.get('Authorization')
-    if user_id is None:
+    if not user_id or not user_id.isdigit():
         abort(400)
     key_id = int(user_id)
-    print "key_id", key_id
-    user= ndb.Key(UserModel, key_id).get()
-    print "user", user
-    return user
+    return ndb.Key(UserModel, key_id).get()
+
+from functools import wraps
+def admin_required(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        auth_user = get_user(request)
+        print "authuser", auth_user
+        if not auth_user or not auth_user.admin:
+            abort(403)
+        return func(*args, **kwargs)
+    return wrapper
 
 class AdminUserAPI(restful.Resource):
     '''GET, PUT, DELETE on _other_ users'''
+    method_decorators = [admin_required]
+
     @marshal_with(user_fields)
     def get(self, key_id):
         user = ndb.Key(UserModel, key_id).get()
@@ -91,6 +99,7 @@ class AdminUserAPI(restful.Resource):
         return make_response("", 204)
 
 class AdminUserListAPI(restful.Resource):
+    method_decorators = [admin_required]
     user_list_fields = {'users': fields.List(fields.Nested(user_fields))}
 
     @marshal_with(user_list_fields)
@@ -122,7 +131,7 @@ class UserAPI(restful.Resource):
     def post(self):
         user = get_user(request)
         if user is not None:
-            abort(405)
+            abort(403)
         args = self.post_parser.parse_args()
         user = UserModel(**args)
         user.put()
